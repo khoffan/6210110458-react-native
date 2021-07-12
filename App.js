d2c9488abcfe0955fd606e9ef47366db250f76ca
khoffan <6210110458@psu.ac.th>
2021-07-12 09:25:04 +0700

    part 1

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text>Hi app Weater !</Text>
       <StatusBar style="auto" />
     </View>
   );
