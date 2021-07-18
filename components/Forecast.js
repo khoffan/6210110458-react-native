251d605285adb482eb491453660d455fb0f01533
khoffan <6210110458@psu.ac.th>
2021-07-18 20:06:56 +0700

    test 3

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Text , View } from 'react-native';
+import { Text , View ,StyleSheet} from 'react-native';
 
 export default function Forecast(props) {
     return (
         <View >
-            <Text>{props.main}</Text>
-            <Text>{props.description}</Text>
-            <Text>{props.temp}</Text>
-            <Text>°C</Text>
+            <Text style={styles.text}>{props.main}</Text>
+            <Text style={styles.text}>{props.description}</Text>
+            <Text style={styles.text}>{props.temp}</Text>
+            <Text style={styles.text}>°C</Text>
         </View>
     );
 }
    
+const styles = StyleSheet.create({
+    text:{
+        margin: 10 ,
+        alignItems:'center',
+        fontSize: 20,
+        flex:1,
+        flexDirection:'column'
+    },
+     
+})
