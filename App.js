d26056ad24a473e2c2d12fd7fd80f25a5a4083a4
khoffan <6210110458@psu.ac.th>
2021-07-18 15:06:28 +0700

    main

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Weather zipCode="11506666"/>
+      <Weather zipCode="90110"/>
       <StatusBar style="auto" />
     </View>
   );
--- components/Weather.js
+++ components/Weather.js
@@ -1,12 +1,29 @@
-import React ,{ useState} from 'react';
+import React ,{ useState , useEffect} from 'react';
 import Forecast from './Forecast';
 import { Text , View ,ImageBackground ,StyleSheet} from 'react-native';
 
-export default function Weather(props) { 
+export default function Weather(props) {
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=fd68c0f2039c5a25f666a9ff374bc93e`)
+            .then((response) => response.json())
+            .then((json) => {
+                setForecastInfo({
+                    main: json.weather[0].main,
+                    description: json.weather[0].description,
+                    temp: json.main.temp});
+                })
+            .catch((error) => {
+                console.warn(error);
+            });
+        }
+    }, [props.zipCode])
+        
     const [forecastInfo , setForecastInfo] = useState({
         main: 'MAIN',
         description: 'description',
-        temp: 25
+        temp: ''
     })
     return (
         <View>
@@ -21,8 +38,7 @@ export default function Weather(props) {
 
 const styles = StyleSheet.create({
     backdrop: {
-        flexDirection:'row',
-        justifyContent:'center',
+        alignItems:'center',
         width: '100%',
         height: '100%'
     },
